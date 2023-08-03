3c210fb5fb79a7784ed214c6ce6c9eefa8df2447
DillTheBeast <dillonmaltese@gmail.com>
2023-08-03 14:07:51 -0400

    Update virus.py

--- virus.py
+++ virus.py
@@ -2,33 +2,61 @@ from PIL import Image, ImageTk
 import tkinter as tk
 from multiprocessing import Process, freeze_support
 from random import randint
+import pyautogui as p
+import vlc
+import pafy
 
 
-def show_image(i):
-    img = Image.open('baby.webp')
+def showImage(i):
+    img = Image.open("baby.jpg")
     window = tk.Tk()
-    window.title(f"Window {i}")
+    window.title("?")
     tk_img = ImageTk.PhotoImage(img)
     label = tk.Label(window, image=tk_img)
     label.pack()
 
-    # Set the window's position
-    x = randint(1, 1920 - 800)
-    y = randint(1, 1080 - 898)
+    x = randint(1, 1920 - 459)
+    y = randint(1, 1050 - 466)
+
     window.geometry(f"+{x}+{y}")
-
     window.mainloop()
+    jerry()
 
-def main():
-    # Create 3 separate windows and display the image
+
+def showVideo():
+    # url of the video
+    url = "https://www.youtube.com/watch?v=1yNfzVABvCM"
+
+    # creating pafy object of the video
+    video = pafy.new(url)
+
+    # getting best stream
+    best = video.getbest()
+
+    # creating vlc media player object
+    media = vlc.MediaPlayer(best.url)
+
+    # start playing video
+    media.play()
+
+
+def jerry():
     processes = []
     for i in range(3):
-        process = Process(target=show_image, args=(i + 1,))
+        process = Process(target=showImage, args=(i + 1,))
         process.start()
         processes.append(process)
-
     for process in processes:
-        process.join()
+        for i in range(3):
+            x = randint(1, 1920)
+            y = randint(1, 1080)
+            p.moveTo(x, y, 1)
+    process.join()
+
+
+def main():
+    # jerry()
+    showVideo()
 
 
 if __name__ == '__main__':
