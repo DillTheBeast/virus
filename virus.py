7b2a010348662aa8609c6b6926c2576c0acbd4cc
DillTheBeast <dillonmaltese@gmail.com>
2023-08-03 09:09:03 -0400

    sad

--- virus.py
+++ virus.py
@@ -1,23 +1,27 @@
 import pyautogui as p
 from random import randint
 import tkinter as tk
-from PTL import Image, ImageTk
+from PIL import Image, ImageTk
 
-for i in range(20):
-    x = randint(1, 1920)
-    y = randint(1, 1080)
-    p.moveTo(x, y, 0.2)
-    
+
+def showImage():
+    # x = randint(1, 1920)
+    # y = randint(1, 1080)
+    # p.moveTo(x, y, 0.2)
+
     root = tk.Tk()
-    root.title = ("?")
+    root.title("?")
 
-    # Load the image 
+    # Load the image
     image_path = "baby.webp"
     img = Image.open(image_path)
-    photo = ImageTk.PhotoImage(img)   
-    
+    photo = ImageTk.PhotoImage(img)
+
     # Create a label to display the image
     label = tk.Label(root, image=photo)
     label.pack()
 
     root.mainloop()
+
+
+showImage()
