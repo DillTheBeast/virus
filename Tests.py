893e0c08f7bd228f60782e15199d007d55a283c4
DillTheBeast <dillonmaltese@gmail.com>
2023-08-18 14:26:14 -0400

    sa

--- Tests.py
+++ Tests.py
@@ -2,16 +2,19 @@ import os
 from email.message import EmailMessage
 import ssl
 import smtplib
+from random import randint
 
 emailer = "logger274@gmail.com"
 emailerPassword = "aaybvlfbvjdjafsv"
-receiver = "dillonmaltese@gmail.com"
+receiver = "ariellewmaltese@gmail.com"
 
-subject = 'Subject Test'
-body = """
-Body
-Test
-"""
+num = randint(1, 10000000)
+
+subject = 'I am a bot'
+# body = """
+# Test
+# """
+body = f"Your random number is: {num}"
 
 em = EmailMessage()
 em['From'] = emailer
@@ -20,7 +23,6 @@ em['Subject'] = subject
 em.set_content(body)
 
 context = ssl.create_default_context()
-
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
     smtp.login(emailer, emailerPassword)
